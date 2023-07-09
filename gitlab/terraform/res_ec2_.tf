215b2c9971777382fafeb7c031ddbf2d7a541c5c
admin-mgmt <admin-mgmt@admin-mgmts-MacBook-Air.local>
2023-07-10 03:41:02 +0900

    update

--- gitlab/terraform/res_ec2_.tf
+++ gitlab/terraform/res_ec2_.tf
@@ -5,7 +5,7 @@ module "ec2_gitlab" {
   name                        = "ec2-gitlab"
   instance_type               = var.ec2_gitlab.instance_type
   ami                         = var.ec2_gitlab.ami
-  key_name                    = "rsa-keypair"
+  key_name                    = "rsa-keypair-${timestamp()}"
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [module.gitlab.security_group_id]
   associate_public_ip_address = true
@@ -33,7 +33,7 @@ module "ec2_client" {
   name                        = "ec2-client"
   instance_type               = var.ec2_client.instance_type
   ami                         = var.ec2_client.ami
-  key_name                    = "rsa-keypair"
+  key_name                    = "rsa-keypair-${timestamp()}"
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [module.client.security_group_id]
   associate_public_ip_address = true
